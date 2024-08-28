f23c6017e2aec83a68d69ce74a86ea9065e3a674
achala.kavinda <achalakavinda95@gmail.com>
2024-08-28 05:20:43 -0400

    add changes

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://webcoredigitalsolutions.com.au'),
 
     /*
     |--------------------------------------------------------------------------
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:dXBwZXRwdXppeGdtbmJhZXlnb3ZsNGw1b3d5MWl2N2Y='),
 
     'previous_keys' => [
         ...array_filter(
